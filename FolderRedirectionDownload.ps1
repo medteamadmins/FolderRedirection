aa2fe037340fc123f691c593c7b16cb8258c1e24
medteamadmins <143048010+medteamadmins@users.noreply.github.com>
2023-08-24 00:11:53 -0400

    Update FolderRedirectionDownload.ps1
    Added the folder validation/creation steps at the start

--- FolderRedirectionDownload.ps1
+++ FolderRedirectionDownload.ps1
@@ -1,4 +1,15 @@
-$FolderRedirection = "C:\ProgramData\TMT"
+# Validate the root path
+    New-Item C:\CheckingRootPath.txt
+    if(!(Test-Path "D:\$($env:USERNAME)" -PathType Container))
+    {
+    New-Item -Path "D:\$($env:USERNAME)" -ItemType Directory -Force
+    Get-ACL $env:USERPROFILE | Set-Acl D:\$($env:USERNAME)
+    New-Item -Path "D:\$($env:USERNAME)\Downloads" -ItemType Directory -Force
+    New-Item -Path "D:\$($env:USERNAME)\Documents" -ItemType Directory -Force
+    New-Item -Path "D:\$($env:USERNAME)\Desktop" -ItemType Directory -Force
+    }
+    
+    $FolderRedirection = "C:\ProgramData\TMT\FolderRedirection"
 If (Test-Path $FolderRedirection) {
     Write-Output "$FolderRedirection exists. Skipping."
 }
@@ -9,7 +20,7 @@ Else {
     Write-Output "The folder $FolderRedirection was successfully created."
 }
 
-$templateFilePath = "C:\ProgramData\TMT\FolderRedirection.ps1"
+$templateFilePath = "C:\ProgramData\TMT\FolderRedirection\FolderRedirection.ps1"
 
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/medteamadmins/FolderRedirection/FolderRedirection.ps1" -OutFile "C:\ProgramData\TMT\FolderRedirection\FolderRedirection.ps1"
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/medteamadmins/FolderRedirection/KnownFolderPathPS5.ps1" -OutFile "C:\ProgramData\TMT\FolderRedirection\KnownFolderPathPS5.ps1"
